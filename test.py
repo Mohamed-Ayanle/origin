b41a81d86911381a9cd2758625f660123753a033
Mohamed Ayanle <mohanayanle@gmail.com>
2023-08-19 14:11:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 #Test
 name= input("what's your name? ").strip().title()
 print("Hello,", name)
+# Welcome paragraph
+print ("Welcome")
 
+#Calculator
 x = int(input("What's x? "))
 y = int(input("What's y? "))
 
+#Result
 print(x*y)
