bb30b8cd0009626c05c043707cdfa69615600a1c
Mohamed Ayanle <mohanayanle@gmail.com>
2023-08-20 14:28:14 +0300

    Add grade

---
+++ grade.py
@@ -0,0 +1,12 @@
+Score = int(input("Score: "))
+
+if Score >= 90:
+        print("Grade A")
+elif Score >= 80:
+        print("Grade B")
+elif Score >= 70:
+        print("Grade C")
+elif Score >= 60:
+        print("Grade D")
+else:
+        print("Grade F")
